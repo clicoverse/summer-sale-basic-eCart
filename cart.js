cd5b9846cba9937866264ece4b11a3a6071175dc
clicoverse <mywebchamp@gmail.com>
2023-08-20 13:24:51 +0530

    product price updated done in cart section

--- cart.js
+++ cart.js
@@ -11,15 +11,18 @@ function purchaseButtonCodition() {
   }
 }
 
+let total = 0;
 function buyThisNow(target) {
-  let priceValue = target.childNodes[3].childNodes[5].innerText.split(" ");
-  let price = parseFloat(priceValue[1]); // get price of the product
   let prodcutName = target.childNodes[3].childNodes[3].innerText; // get product name from product display
   const p = document.createElement("p");
   p.innerText = prodcutName;
   cartItmesDisplay.appendChild(p); //product name displayed to the cart
-  // console.log(prodcutName);
   purchaseButtonCodition();
+  let priceValue = target.childNodes[3].childNodes[5].innerText.split(" ");
+  let price = parseFloat(priceValue[1]); // get price of the product
+  total = parseFloat(total) + parseFloat(price);
+  document.getElementById("total-price").innerText = "₹ " + total.toFixed(2); //total price updated to the cart section
+  console.log(typeof total);
 }
 
 purchaseButtonCodition();
